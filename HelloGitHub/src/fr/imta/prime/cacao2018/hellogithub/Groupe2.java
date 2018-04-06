3c0d43d3dfeafaa4667150871147559834fd8f1c
Agathe Chevalier <Agathe Chevalier@172.17.3.139>
2018-04-06 17:59:13 +0200

    vide

---
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Groupe2.java
@@ -0,0 +1,13 @@
+package fr.imta.prime.cacao2018.hellogithub;
+
+public class Groupe2 {
+	
+	public static void printer() {
+		System.out.println("AgatheChevalier est Agathe Chevalier");
+	}
+	
+	public static void main(String[] args) {
+		printer();
+		
+	}
+}
