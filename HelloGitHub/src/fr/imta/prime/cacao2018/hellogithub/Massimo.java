f8711e3a52aa82eed7b952e231c69d8d97f6a6ca
L3rak <L3rak@172.17.2.147>
2018-04-06 17:40:59 +0200

    efheuhf

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Massimo.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Massimo.java
@@ -3,5 +3,7 @@ package fr.imta.prime.cacao2018.hellogithub;
 public class Massimo {
 	public static void main(String[] args) {
 	System.out.print("Léopold Petitjean");
+	System.out.print("Karel Kédémos");
+
 	}
 }
