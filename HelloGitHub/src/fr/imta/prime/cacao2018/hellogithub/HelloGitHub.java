05b48530d98759a6e796d34581cb52406bba2bfa
Muikhi <Muikhi@172.17.2.24>
2018-04-06 17:51:50 +0200

    Morgane

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
@@ -4,6 +4,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("C'est Cerbin (Grégoire) !");
+		System.out.println("C'est Muikhi (Morgane) !");
 	}
 
 }
