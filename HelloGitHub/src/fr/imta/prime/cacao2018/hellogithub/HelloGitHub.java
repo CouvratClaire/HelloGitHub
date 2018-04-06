cc08d7288f5abe454f46b299755d4a0f0ba6cb68
guillaume <guillaume@GS-T460p>
2018-04-06 17:32:42 +0200

    Gui

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
@@ -1,9 +1,13 @@
 package fr.imta.prime.cacao2018.hellogithub;
 
 public class HelloGitHub {
-
+	
+	public static void printer() {
+		System.out.println("GuillaumeSa est Guillaume Sallé");
+	}
+	
 	public static void main(String[] args) {
-
+		printer();
 	}
 
 }
