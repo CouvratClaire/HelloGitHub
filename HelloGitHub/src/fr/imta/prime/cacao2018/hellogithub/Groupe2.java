559daa90345a0e8e5fb9059b4a8763fd31c18845
RomBe <RomBe@172.17.3.32>
2018-04-06 18:04:38 +0200

    Romain

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Groupe2.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Groupe2.java
@@ -5,6 +5,7 @@ public class Groupe2 {
 	public static void printer() {
 		System.out.println("AgatheChevalier est Agathe Chevalier");
 		System.out.println("alexandrebigotcacao est Alexandre Bigot");
+		System.out.println("rombernard est Romain Bernard");
 	}
 	
 	public static void main(String[] args) {
