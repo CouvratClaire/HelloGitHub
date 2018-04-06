b5a8dbd77bd43c449eff4615b4c3c0ab869f26f4
clair <clair@172.18.18.181>
2018-04-06 18:03:32 +0200

    Change

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
@@ -3,9 +3,7 @@ package fr.imta.prime.cacao2018.hellogithub;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("C'est Cerbin (Grégoire) !");
-		System.out.println("C'est Muikhi (Morgane) !");
-		System.out.println("C'est CouvratClaire (Claire) !");
+	
 	}
 
 }
