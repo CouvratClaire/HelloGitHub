58c9274697331ff86d82dffb641f0e475434ebf4
micka <micka@172.17.3.226>
2018-04-12 09:45:21 +0200

    Salut sa va

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Groupe4.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Groupe4.java
@@ -5,6 +5,7 @@ public class Groupe4 {
 	public static void printer() {
 		System.out.println("norigolepas est Noémie Rigaut");
 		System.out.println("carlivoix est Charles Vivoix");
+		System.out.println("mabdealy est Mickaël Abdealy");
 	}
 	
 	public static void main(String[] args) {
